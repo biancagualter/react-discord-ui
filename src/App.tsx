c28952457199413697c3589ce4f51a5f689ea03b
Bianca Gualter <bncgualter@gmail.com>
2021-03-09 01:43:37 -0300

    create serverlist component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyles'
 import Layout from './components/Layout';
+import ServerList from './components/ServerList';
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <Layout />
+      <ServerList />
     </>
   );
 }
---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container, Separator } from './styles'
+
+const ServerList: React.FC = () => {
+    return (
+        <Container>
+            <ServerButton isHome/>
+            <Separator />
+            <ServerButton hasNotifications />
+            <ServerButton mentions={2} />
+            <ServerButton />
+            <ServerButton mentions={6}/>
+            <ServerButton />
+            <ServerButton  hasNotifications />
+            <ServerButton />
+            <ServerButton />
+        </Container>
+    )
+};
+
+export default ServerList
