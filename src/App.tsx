1c4b8110e4198f134b047f994ae89e4e3a1e0dc9
Bianca Gualter <bncgualter@gmail.com>
2021-03-09 11:12:55 -0300

    create channel info component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyles'
 import Layout from './components/Layout';
-import ServerList from './components/ServerList';
 
 function App() {
   return (
