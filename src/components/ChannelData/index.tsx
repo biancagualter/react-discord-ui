f0ce31209ceada85fa451f3cd7a511eb225e611d
Bianca Gualter <bncgualter@gmail.com>
2021-03-13 16:23:14 -0300

    create channel message component

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
-import { Container, Messages, InputWrapper, Input, InputIcon} from './styles'
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles'
+import ChannelMessage, { Mention } from '../ChannelMessage'
 
 const ChannelData: React.FC = () => {
+    let dateinfo = new Date()
+    const day = dateinfo.toLocaleDateString()
+
+    const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>
+
+    useEffect(() => {
+        const div = messagesRef.current
+
+        if(div) {
+            div.scrollTop = div.scrollHeight
+        }
+    }, [messagesRef])
+
     return (
         <Container>
-            <Messages />
+            <Messages ref={messagesRef}>
+                <ChannelMessage 
+                    author="Beltrano de Tal"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Bianca Gualter"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Beltrano de Tal"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Sicrano de Tal"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Sicrano de Tal"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Beltrano de Tal"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Sicrano de Tal"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Bianca Gualter"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Beltrano de Tal"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Sicrano de Tal"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Bianca Gualter"
+                    date={day}
+                    content="Isso é uma mensagem" 
+                />
+                <ChannelMessage 
+                    author="Fulano de Tal"
+                    date={day}
+                    content={
+                        <>
+                            <Mention>@Bianca Gualter </Mention>
+                            It`s Britney, Bitch!
+                        </>
+                    }
+                    hasMention
+                    isBot 
+                />
+            </Messages>
             <InputWrapper>
                 <Input type="text" placeholder="Conversar em #chat-livre"/>
                 <InputIcon />
--- src/components/ChannelData/styles.ts
+++ src/components/ChannelData/styles.ts
@@ -13,11 +13,12 @@ export const Container = styled.div `
 export const Messages = styled.div `
     display: flex;
     flex-direction: column;
-    max-height: calc(100vh -46px -68px);
+    max-height: calc(100vh - 46px - 68px);
     padding: 20px 0;
     overflow-y: scroll;
 
     ::-webkit-scrollbar {
+        
         width: 8px;
     }
 
@@ -27,7 +28,7 @@ export const Messages = styled.div `
     }
 
     ::-webkit-scrollbar-track {
-        background-color: var(--secondary);
+        background-color: var(--primary);
     }
 `
 export const InputWrapper = styled.div `
